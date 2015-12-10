64851b6129b38e8ffffddc3fa46ea52b270c71a1
Dzmitry Rekuts <dzmitry_rekuts@epam.com>
2015-12-10 18:23:48 +0300

    Corrected names. Delete "yield".

--- EuclidAlgoLogik.Tests/EuclidLogikTests.cs
+++ EuclidAlgoLogik.Tests/EuclidLogikTests.cs
@@ -14,30 +14,6 @@ namespace EuclidAlgoLogik.Tests
     public class EuclidLogikTests
     {
 
-        public static IEnumerable TestCases
-        {
-            get
-            {
-                yield return new TestCaseData(new int[] { -25, -1075, 3, 58, 25 }).Returns(1);
-                yield return new TestCaseData(new int[] { -25, -1075, -3, -58, -25 }).Returns(1);
-                yield return new TestCaseData(new int[] { 25, 1075, 3, 58, 25 }).Returns(1);
-                yield return new TestCaseData(new int[] { 0, 0, 0, 0, 0 }).Returns(0);
-                yield return new TestCaseData(new int[] { 0, 0, 33, 0, 0 }).Returns(33);
-                yield return new TestCaseData(new int[] { 3, 333, 33, 27, 9 }).Returns(3);
-                yield return new TestCaseData(new int[] { -3, -333, -33, -27, -9 }).Returns(3);
-                yield return new TestCaseData(new int[] { 3, -333, 33, -27, -9 }).Returns(3);
-            }
-        }
-        [Test, TestCaseSource(nameof(TestCases))]
-        public double NewtonMetode_PositiveTests_Params(int [] arr)
-        {
-            var timer = new Stopwatch();
-            return EuclidAlgo.EuclidAlgoMethod(timer,arr);
-        }
-
-
-
-
         [TestCase(24, 12, Result = 12)]
         [TestCase(24, -12, Result = 12)]
         public int  Euclid_PositiveTests(int a, int b)
