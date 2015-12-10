cdc55d8156aa1d095e32576d9ae869f7e9a6bdc3
Dzmitry Rekuts <dzmitry_rekuts@epam.com>
2015-12-10 18:38:24 +0300

    "yield" come back )

--- EuclidAlgoLogik.Tests/EuclidLogikTests.cs
+++ EuclidAlgoLogik.Tests/EuclidLogikTests.cs
@@ -14,9 +14,34 @@ namespace EuclidAlgoLogik.Tests
     public class EuclidLogikTests
     {
 
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { -25, -1075, 3, 58, 25 }).Returns(1);
+                yield return new TestCaseData(new int[] { -25, -1075, -3, -58, -25 }).Returns(1);
+                yield return new TestCaseData(new int[] { 25, 1075, 3, 58, 25 }).Returns(1);
+                yield return new TestCaseData(new int[] { 3, 333, 33, 27, 9 }).Returns(3);
+                yield return new TestCaseData(new int[] { -3, -333, -33, -27, -9 }).Returns(3);
+                yield return new TestCaseData(new int[] { 3, -333, 33, -27, -9 }).Returns(3);
+
+                yield return new TestCaseData(new int[] { 0, 0, 0, 0, 0 }).Throws(typeof(System.ArgumentException));
+                yield return new TestCaseData(new int[] { 0, 0, 33, 0, 0 }).Throws(typeof(System.ArgumentException));
+
+            }
+        }
+
+        [Test, TestCaseSource(typeof(EuclidLogikTests), "TestCases")]
+        public int Euclid_PositiveTests_ParamsArray(int[] arr)
+        {
+            var timer = new Stopwatch();
+            return EuclidAlgo.EuclidAlgoMethod(timer, arr);
+        }
+
         [TestCase(24, 12, Result = 12)]
         [TestCase(24, -12, Result = 12)]
-        public int  Euclid_PositiveTests(int a, int b)
+        [TestCase(0, -12, ExpectedException = typeof(ArgumentException))]
+        public int Euclid_PositiveTests(int a, int b)
         {
             var timer = new Stopwatch();
             return EuclidAlgo.EuclidAlgoMethod(timer, a, b);
@@ -31,7 +56,9 @@ namespace EuclidAlgoLogik.Tests
         }
 
 
-        [TestCase(12,12,12,12,12,12 , Result = 12)]
+        [TestCase(12, 12, 12, 12, 12, 12, Result = 12)]
+        [TestCase(256, 24, 958, 8, 682, 984, Result = 2)]
+
         public int Euclid_PositiveTests_Params(int a, int b, int c, int d, int e, int f)
         {
             var timer = new Stopwatch();
